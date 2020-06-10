78627f723677e4f98f273e0d2189a3d5f9c2e30c
Cauã Henrique <cauah6002@gmail.com>
2020-06-10 13:37:47 -0300

    Monitorador

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import socketIOClient from "socket.io-client"
 
 function App() {
+
+  const [quantProdutos, setQuantProdutos] = useState('')
+  const [quantCategorias, setQuantCategorias] = useState('')
+
+  const socket = socketIOClient('http://localhost:3001')
+
+  fetch('http://localhost:3001/quantprodutos')
+    .then(res => res.json())
+    .then(quantidade => setQuantProdutos(quantidade['0'].quantidade))
+  fetch('http://localhost:3001/quantcategorias')
+    .then(res => res.json())
+    .then(quantidade => setQuantCategorias(quantidade['0'].quantidade))
+
+  useEffect(() => {
+    socket.on('quantidade-produtos', data => {
+      fetch('http://localhost:3001/quantprodutos')
+        .then(res => res.json())
+        .then(quantidade => setQuantProdutos(quantidade['0'].quantidade))
+    })
+  }, [])
+
+  useEffect(() => {
+    socket.on('quantidade-categorias', data => {
+      fetch('http://localhost:3001/quantcategorias')
+        .then(res => res.json())
+        .then(quantidade => setQuantCategorias(quantidade['0'].quantidade))
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="product-container">
+        <h2>Quantidade de Produtos</h2>
+        <h1>{quantProdutos}</h1>
+      </div>
+      <div className="categories-container">
+        <h2>Quantidade de Categorias</h2>
+        <h1>{quantCategorias}</h1>
+      </div>
     </div>
   );
 }
